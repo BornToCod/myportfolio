fcb2292d646ea711d47f3ee22fcefd2c52d4c8b2
BornToCod <ali02heddad@gmail.com>
2023-12-02 18:32:27 +0100

    Added my project1

--- public/script.js
+++ public/script.js
@@ -2,6 +2,30 @@ const menuButton = document.getElementById('menu-toggle');
 const navMenu = document.getElementById('nav-menu');
 const navMenuSmall = document.getElementById('nav-menu-small');
 
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all project cards
+  const projectCards = document.querySelectorAll(".animate-on-scroll");
+
+  // Set up the Intersection Observer
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 0.5 } // Adjust the threshold as needed
+  );
+
+  // Observe each project card
+  projectCards.forEach((card) => {
+    observer.observe(card);
+  });
+});
+
 // ScrollReveal().reveal('.project-card', { delay: 200, duration: 500, easing: 'ease-in-out' });
 
 menuButton.addEventListener('click', () => {
@@ -15,6 +39,8 @@ window.addEventListener('resize', () => {
         navMenuSmall.classList.remove('flex', 'flex-row');
     }
 });
+
+
 
 // const token='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';
 // // Your GitHub API fetch code (as shown in the previous response)
@@ -60,7 +86,7 @@ window.addEventListener('resize', () => {
 
 
 
-const token = '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';
+const token = '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';
 const apiUrl = `https://api.github.com/users/${'BornToCod'}/repos`;
 
 fetch(apiUrl, {
@@ -81,8 +107,8 @@ fetch(apiUrl, {
       // Loop through your GitHub projects and create a card for each
       data.forEach((project) => {
         const card = document.createElement('div');
-        card.classList.add('project-card','max-w-sm', 'mx-3', 'my-3', 'h-auto', 'rounded-xl', 'shadow-md', 'overflow-hidden', 'hover:shadow-lg', 'hover:transform' ,'hover:scale-105' ,'transition-transform','delay-[300ms]','duration-[600ms]', 'taos:translate-y-[200px]', 'taos:opacity-0');
-        card.setAttribute('data-taos-offset','300');
+        card.classList.add('project-card','animate-on-scroll','max-w-sm', 'mx-3', 'my-3', 'h-auto', 'rounded-xl', 'shadow-md', 'overflow-hidden', 'hover:shadow-lg', 'hover:transform' ,'hover:scale-105' );
+        card.setAttribute('data-aos','fade-up');
 
         const title = document.createElement('h2');
         title.textContent = project.name;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,5 @@
 module.exports = {
-  purge: ["./public/**/*.html"],
-  safelist: [
-    '!duration-[0ms]',
-    '!delay-[0ms]',
-    'html.js :where([class*="taos:"]:not(.taos-init))'
-  ],
-  content: {
-    relative: true,
-    transform: (content) => content.replace(/taos:/g, ''),
-    files: ['./src/*.{html,js}'],
-  },
-  theme: {
-    extend: {},
-  },
-  variants: {},
-  plugins: [
-    require('taos/plugin')
-  ],
-  autoprefixer: {},
+  content: ["./public/**/*.html,js"],
+  theme: { extend: {}, },
+   plugins: [], 
 };
